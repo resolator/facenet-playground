fbd102c85e50b1dbdbe73e19eaae38801ec8d99a
Vladyslav <v.dusiak@gmail.com>
2020-07-23 21:37:42 +0300

    FaceNet wrapper documented

--- scripts/facenet.py
+++ scripts/facenet.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*
-"""
-Common functions.
-"""
+"""FaceNet inference implementation."""
 import cv2
 import numpy as np
 import tensorflow as tf
 
 
 class FaceNet:
+    """FaceNet inference implementation."""
+
     def __init__(self, model_path):
+        """Class constructor.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to a trained FaceNet model.
+
+        """
         # model loading
         graph_def = tf.GraphDef()
         with open(model_path, 'rb') as f:
@@ -28,14 +36,41 @@ class FaceNet:
         self._emb_op = graph.get_tensor_by_name('embeddings:0')
 
     @staticmethod
-    def preprocess(cv_rgb_img):
+    def _preprocess(cv_rgb_img):
+        """Preprocess image before forward pass.
+
+        Parameters
+        ----------
+        cv_rgb_img : np.array
+            3-dimensional array (image).
+
+        Returns
+        -------
+        np.array
+            Preprocessed image.
+
+        """
         mean = np.mean(cv_rgb_img)
         std = np.std(cv_rgb_img)
         std_adj = np.maximum(std, 1.0 / np.sqrt(cv_rgb_img.size))
         res = np.multiply(np.subtract(cv_rgb_img, mean), 1 / std_adj)
+
         return res
 
     def calc_embeddings(self, cv_bgr_imgs):
+        """Calculate embeddings for given images.
+
+        Parameters
+        ----------
+        cv_bgr_imgs : array-like
+            Array with BGR images (supposed to be read with cv2.imread()).
+
+        Returns
+        -------
+        np.array
+            An array with calculated embeddings for each image.
+
+        """
         # bgr to rgb
         cv_rgb_imgs = [x[:, :, ::-1] for x in cv_bgr_imgs]
 
@@ -46,7 +81,7 @@ class FaceNet:
                         for x in cv_rgb_imgs]
 
         # preprocess
-        preprocessed_imgs = [self.preprocess(x) for x in resized_imgs]
+        preprocessed_imgs = [self._preprocess(x) for x in resized_imgs]
 
         # calculate embeddings
         embeddings = self._sess.run(self._emb_op,
