d90106c9940af642ce055ec3d71bac67e3633b53
Vladyslav <v.dusiak@gmail.com>
2020-07-26 21:46:24 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-sudo apt update && sudo apt install -y git-lfs python3-dev python3-venv
+sudo apt update && sudo apt install -y git-lfs python3-dev python3-venv python3-opencv
 git-lfs pull
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+git clone https://github.com/peteryuX/retinaface-tf2.git
+
