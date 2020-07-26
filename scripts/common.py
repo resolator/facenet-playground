1dab06585213be7b5218cc9efba07173a56576ea
Vladyslav <v.dusiak@gmail.com>
2020-07-26 21:46:11 +0300

    Added additional script for faces cutting

--- scripts/common.py
+++ scripts/common.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*
 """Commonn functions."""
+import cv2
+
+import numpy as np
 import tensorflow as tf
+
+from tqdm import tqdm
+from functools import partial
+from multiprocessing import Pool
+from pathlib import Path
 
 
 def set_memory_growth():
@@ -17,3 +25,77 @@ def set_memory_growth():
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
+
+
+def pad_img(img, max_steps=32):
+    """Pad image to suitable shape.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        Read image.
+    max_steps : int
+        The number from train config (default is 32).
+
+    Returns
+    -------
+    tuple
+        Padded image and padding params for recovering.
+
+    """
+    img_h, img_w, _ = img.shape
+
+    img_pad_h = 0
+    if img_h % max_steps > 0:
+        img_pad_h = max_steps - img_h % max_steps
+
+    img_pad_w = 0
+    if img_w % max_steps > 0:
+        img_pad_w = max_steps - img_w % max_steps
+
+    padd_val = np.mean(img, axis=(0, 1)).astype(np.uint8)
+    img = cv2.copyMakeBorder(img, 0, img_pad_h, 0, img_pad_w,
+                             cv2.BORDER_CONSTANT, value=padd_val.tolist())
+    pad_params = (img_h, img_w, img_pad_h, img_pad_w)
+
+    return img, pad_params
+
+
+def read_dir(imgs_dir, rs_size=(150, 150)):
+    for img_path in Path(imgs_dir.decode('utf-8')).glob('*'):
+        img_original = cv2.imread(str(img_path))
+        img = img_original[:, :, ::-1]
+        img = cv2.resize(img, rs_size)
+        img, _ = pad_img(img)
+
+        yield img.astype(np.float32), img_original, int(img_path.stem)
+
+
+def get_ds(imgs_dir, bs=1, rs_size=(150, 150)):
+    """
+
+    Parameters
+    ----------
+    imgs_dir : str
+    bs
+    rs_size
+    workers
+
+    Returns
+    -------
+    tf.data.Dataset
+        Created dataset.
+
+    """
+    test_img = np.zeros((*rs_size, 3))
+    test_img, _ = pad_img(test_img)
+
+    ds = tf.data.Dataset.from_generator(
+        partial(read_dir, rs_size=rs_size),
+        args=[imgs_dir],
+        output_types=(tf.float32, tf.int32),
+        output_shapes=(test_img.shape, ())
+    )
+    ds = ds.batch(bs)
+
+    return ds
---
+++ scripts/cut_faces.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+import cv2
+import sys
+import argparse
+
+import tensorflow as tf
+
+from tqdm import tqdm
+from pathlib import Path
+from face_detector import FaceDetector
+from common import set_memory_growth, get_ds
+
+
+def get_args():
+    """Arguments parser."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--images-dir', type=Path, required=True,
+                        help='Path to dir with images only for search in '
+                             'database.')
+    parser.add_argument('--detector-model-dir',
+                        default=sys.path[0] + '/../data/face_detector/',
+                        help='Path to dir with tf saved model for detector.')
+    parser.add_argument('--bs', type=int, default=1,
+                        help='Batch size.')
+    parser.add_argument('--save-to', type=Path, required=True,
+                        help='Path to save dir to save cropped images.')
+
+    return parser.parse_args()
+
+
+def main():
+    """Application entry point."""
+    args = get_args()
+    set_memory_growth()
+    args.save_to.mkdir(exist_ok=True, parents=True)
+
+    loaded = tf.saved_model.load(args.detector_model_dir)
+    det = loaded.signatures['serving_default']
+
+    detector = FaceDetector(det)
+
+    imgs_paths = list(args.images_dir.glob('*'))
+    for i in tqdm(range(0, len(imgs_paths), args.bs)):
+        batch = imgs_paths[i:i + args.bs]
+        imgs_raw = [cv2.imread(str(x)) for x in batch]
+        faces = detector.detect_and_cut(imgs_raw)
+
+        for face, old_img_path in zip(faces, batch):
+            new_img_path = args.save_to.joinpath(old_img_path.name)
+            cv2.imwrite(str(new_img_path), face)
+
+
+if __name__ == '__main__':
+    main()
