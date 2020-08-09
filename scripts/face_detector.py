4f4f89ff3c5fe86232b5be938080c398aa3ec885
Vladyslav <v.dusiak@gmail.com>
2020-08-09 18:54:03 +0300

    WIP: face detector code cleaned

--- scripts/face_detector.py
+++ scripts/face_detector.py
@@ -1,131 +1,95 @@
 # -*- coding: utf-8 -*
 """Wrapper for face detection."""
-import cv2
-import numpy as np
 import tensorflow as tf
 
 
 class FaceDetector:
     """Wrapper for face detection."""
 
-    def __init__(self, saved_model_tf):
+    def __init__(self, saved_model_tf, expand_factors=None):
         """Class constructor.
 
         Parameters
         ----------
         saved_model_tf : tf.saved_model
             Loaded TF saved model.
+        expand_factors : array-like
+            Array with TOP, BOTTOM, RIGHT and LEFT expand factors for
+            detections.
 
         """
         self._det = saved_model_tf
+        self._expand_factors = expand_factors
 
-    @staticmethod
-    def _unpad_boxes(boxes, pad_params):
-        """Recover the padded output effect."""
-        img_h, img_w, img_pad_h, img_pad_w = pad_params
-        reshaped_bb = np.reshape(boxes, [-1, 2, 2])
-        recover_xy = reshaped_bb * [(img_pad_w + img_w) / img_w,
-                                    (img_pad_h + img_h) / img_h]
-        boxes = np.reshape(recover_xy, [4])
-
-        return boxes
-
-    @staticmethod
-    def _pad_img(img, max_steps=32):
-        """Pad image to suitable shape.
-
-        Parameters
-        ----------
-        img : numpy.ndarray
-            Read image.
-        max_steps : int
-            The number from train config (default is 32).
-
-        Returns
-        -------
-        tuple
-            Padded image and padding params for recovering.
-
-        """
-        img_h, img_w, _ = img.shape
-
-        img_pad_h = 0
-        if img_h % max_steps > 0:
-            img_pad_h = max_steps - img_h % max_steps
-
-        img_pad_w = 0
-        if img_w % max_steps > 0:
-            img_pad_w = max_steps - img_w % max_steps
-
-        padd_val = np.mean(img, axis=(0, 1)).astype(np.uint8)
-        img = cv2.copyMakeBorder(img, 0, img_pad_h, 0, img_pad_w,
-                                 cv2.BORDER_CONSTANT, value=padd_val.tolist())
-        pad_params = (img_h, img_w, img_pad_h, img_pad_w)
-
-        return img, pad_params
-
-    def _apply(self, inp):
-        """Apply net to input tensor and return bounding box."""
-        outputs = self._det(inp)
-        boxes_batch = outputs['tf_op_layer_CombinedNonMaxSuppression'].numpy()
-        boxes_batch = np.squeeze(boxes_batch, axis=1)  # model returns 1 box
-
-        return boxes_batch
-
-    def detect_and_cut(self, imgs_batch, rs_size=(150, 150)):
+    def detect_and_cut(self, imgs_batch):
         """Detect faces, cut first one and return resulted image.
 
         Parameters
         ----------
-        imgs_batch : array of numpy.ndarray
-            Read RGB images.
-        rs_size : tuple
-            Resize images to this size before forward pass.
+        imgs_batch : tf.Tensor
+            Batch of images.
 
         Returns
         -------
         np.array
-            Cropped face or source image if no face was found.
+            Cropped and resized for FaceNet face
+            or resized source image if no face was found.
 
         """
-        # prepare data
-        rgb_rs_batch = [cv2.resize(x, rs_size) for x in imgs_batch]
-        pad_batch = [self._pad_img(x) for x in rgb_rs_batch]
+        outputs = self._det(imgs_batch)
+        boxes_batch = tf.squeeze(
+            outputs['tf_op_layer_CombinedNonMaxSuppression'], axis=1)
 
-        pad_params = [x[1] for x in pad_batch]
-        imgs_pad = [x[0] for x in pad_batch]
-
-        inp = tf.constant(imgs_pad, dtype=tf.float32)
-        boxes_batch = self._apply(inp)
-
-        # postprocessing
-        boxes_batch = [self._unpad_boxes(x, y)
-                       for x, y in zip(boxes_batch, pad_params)]
-        cropped_imgs = self._crop(imgs_batch, boxes_batch)
+        cropped_imgs = self._tf_crop(imgs_batch, boxes_batch)
 
         return cropped_imgs
 
-    @staticmethod
-    def _crop(images, boxes):
-        """Crop images using boxes and return cropped images."""
+    @tf.function
+    def _tf_crop(self, images, boxes, min_size=(20, 20)):
+        face_shape = (160, 160)  # according to the FaceNet
+        boxes = tf.abs(boxes)
         cropped_imgs = []
-        for img, box in zip(images, boxes):
-            box = np.abs(box)
+
+        for i, (img, box) in enumerate(zip(tf.unstack(images),
+                                           tf.unstack(boxes))):
             # relative x1, y1, x2, y2
-            x1 = int(img.shape[0] * box[0])
-            y1 = int(img.shape[1] * box[1])
-            x2 = int(img.shape[0] * box[2])
-            y2 = int(img.shape[1] * box[3])
+            y1 = img.shape[0] * box[0]
+            x1 = img.shape[1] * box[1]
+            y2 = img.shape[0] * box[2]
+            x2 = img.shape[1] * box[3]
 
-            cropped = img[x1:x2, y1:y2]
+            # expand detection
+            if self._expand_factors is not None:
+                crop_h, crop_w = y2 - y1, x2 - x1
+                y1 = tf.maximum(y1 - crop_h * self._expand_factors[0], 0)
+                x1 = tf.maximum(x1 - crop_w * self._expand_factors[3], 0)
 
-            # if crop are too big
-            if any(np.array(cropped.shape) <= 0):
-                print('WARNING: crop is too big or face not found:')
-                print('Image shape:', img.shape)
-                print('Detected box:', box)
-                cropped_imgs.append(img)
-            else:
-                cropped_imgs.append(img[x1:x2, y1:y2])
+                y2 = tf.minimum(y2 + crop_h * self._expand_factors[1],
+                                img.shape[0])
+                x2 = tf.minimum(x2 + crop_w * self._expand_factors[2],
+                                img.shape[1])
 
-        return cropped_imgs
+            # crop
+            cropped = img
+            if y2 > min_size[0] and x2 > min_size[1]:
+                cropped = tf.image.crop_to_bounding_box(
+                    img,
+                    tf.cast(y1, tf.int32),
+                    tf.cast(x1, tf.int32),
+                    tf.cast(y2 - y1, tf.int32),
+                    tf.cast(x2 - x1, tf.int32)
+                )
+
+            cropped = tf.image.resize(cropped, face_shape)
+
+            # facenet preproc
+            mean = tf.math.reduce_mean(cropped)
+            std = tf.math.reduce_std(cropped)
+            std_adj = tf.maximum(std, 1.0 / tf.sqrt(
+                tf.cast(cropped.shape[0] * cropped.shape[1] * cropped.shape[2],
+                        tf.float32)))
+            res = tf.multiply(tf.subtract(cropped, mean), 1 / std_adj)
+
+            cropped_imgs.append(res)
+
+        return tf.stack(cropped_imgs)
